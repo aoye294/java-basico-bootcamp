9e63a8de242d98b438332d4d96757d54fea01965
aoye204 <aosaote16@gmail.com>
2022-04-20 01:09:25 +0200

    segunda entrega modificada

--- entrega2/SegundaEntrega.java
+++ entrega2/SegundaEntrega.java
@@ -3,9 +3,10 @@ package entrega2;
 public class SegundaEntrega {
     private static double IVA = 0.21;
     public static void main(String[] args) {
-        iva(250);
+       
+        System.out.println("Precio total con IVA: "  + iva(250));
     }
-    private static void iva(int precio){
-        System.out.println("\nPrecio total con IVA: "  + precio + (precio * IVA)+"$\n");
+    private static double iva(float precio){
+        return precio + (precio * IVA);
     }
 }
